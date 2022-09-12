920a03eb72180ddcfa1f7fb82c2f88eca2dbc1e4
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-12 21:36:09 +0300

    Update 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryItemsMarkup(galleryItems);
+
+
+//----Створення розмітки-------------------
+
+function createGalleryItemsMarkup(gallery) {
+
+    const galleryMarkup = gallery.map(({preview, original, description}) =>{
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+    
+    return galleryMarkup;
+};
+
+//---галерея SimpleLightbox---------------
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay: 250});
