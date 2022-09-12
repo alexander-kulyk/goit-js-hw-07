56f2050b2211877cbf50b0b8a0cbb2a4ab1aa99d
alexander-kulyk <kulyk.alexander@gmail.com>
2022-09-12 19:30:47 +0300

    update gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,65 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend',createGallaryItemsMarkup(galleryItems));
+
+galleryContainer.addEventListener('click',onGalleryImagesClick);
+
+//----Створення розмітки-------------------
+
+function createGallaryItemsMarkup(gallary) {
+    
+    const gallaryMarkup = gallary.map(({preview, original, description}) =>{
+        return`
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img 
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}" 
+          alt="${description}">
+        </a>
+      
+      </div>`
+    }).join('');
+    return gallaryMarkup;
+};
+
+
+function onGalleryImagesClick(evt) {
+
+    //--заборона стандартних дій браузера
+    evt.preventDefault();
+
+    //---перевіряю чи елемент має клас gallery__image, якщо ні  - вихожу
+    if (!evt.target.classList.contains('gallery__image')) {
+        return; 
+    };
+    
+
+    //--якщо так запускаю бібліотеку
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">`);
+    
+    instance.show();
+
+
+    //-----Закриття модалки-------------
+    window.addEventListener('keydown', (evt) =>{
+        if (evt.code === 'Escape') {
+            instance.close();
+        };
+    });
+};
+
+
+
+
+
+
+
+
+
